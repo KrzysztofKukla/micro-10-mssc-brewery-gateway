e0b633d71e25f6039c305e4cafe882307318f60d
Krzysztof Kukla <krzys.kukla@gmail.com>
2020-05-17 20:13:20 +0200

    Initial commit

---
+++ src/main/java/pl/kukla/krzys/micro10msscbrewerygateway/Micro10MsscBreweryGatewayApplication.java
@@ -0,0 +1,13 @@
+package pl.kukla.krzys.micro10msscbrewerygateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Micro10MsscBreweryGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Micro10MsscBreweryGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/kukla/krzys/micro10msscbrewerygateway/Micro10MsscBreweryGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kukla.krzys.micro10msscbrewerygateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Micro10MsscBreweryGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
