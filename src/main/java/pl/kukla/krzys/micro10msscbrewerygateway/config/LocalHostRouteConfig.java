700aa7abf101cee26df8cbe3408f7c4729fae10a
Krzysztof Kukla <krzys.kukla@gmail.com>
2020-05-18 21:32:51 +0200

    assignment_16 route for BeerOrderService, closes #3

--- src/main/java/pl/kukla/krzys/micro10msscbrewerygateway/config/LocalHostRouteConfig.java
+++ src/main/java/pl/kukla/krzys/micro10msscbrewerygateway/config/LocalHostRouteConfig.java
@@ -12,13 +12,17 @@ import org.springframework.context.annotation.Configuration;
 public class LocalHostRouteConfig {
 
     @Bean
-    public RouteLocator localHostRoutes(RouteLocatorBuilder builder) {
+    public RouteLocator beerServiceRoute(RouteLocatorBuilder builder) {
         return builder.routes()
             //here we route/redirect paths into 'localhost:8080'
-            .route(r -> r.path("/api/v1/beer*","/api/v1/beer/*", "/api/v1/beerUpc/*")
+            .route(r -> r.path("/api/v1/beer*", "/api/v1/beer/*", "/api/v1/beerUpc/*")
                 //redirect to beerService which is running on 'localhost:8080'
                 .uri("http://localhost:8080")
                 .id("beer-service"))
+            .route(r -> r.path("/api/v1/customers/**")
+                .uri("http://localhost:8081")
+                .id("beer-order-service"))
             .build();
     }
+
 }
