d9d8b6ae8dfb789158112c53982bdc8dcd184b16
Krzysztof Kukla <krzys.kukla@gmail.com>
2020-05-19 23:41:41 +0200

    264 LoadBalancer to use many instances of microservices

--- src/main/java/pl/kukla/krzys/micro10msscbrewerygateway/config/LoadBalancedRoutesConfig.java
+++ src/main/java/pl/kukla/krzys/micro10msscbrewerygateway/config/LoadBalancedRoutesConfig.java
@@ -9,25 +9,26 @@ import org.springframework.context.annotation.Profile;
 /**
  * @author Krzysztof Kukla
  */
-@Profile("!local-discovery")
+@Profile(value = "local-discovery")
 @Configuration
-public class LocalHostRouteConfig {
+public class LoadBalancedRoutesConfig {
 
+    //Load Balance to use many instances of microservices
     @Bean
-    public RouteLocator beerServiceRoute(RouteLocatorBuilder builder) {
+    public RouteLocator loadBalancedRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
             //here we route/redirect paths into 'localhost:8080'
             .route(r -> r.path("/api/v1/beer*", "/api/v1/beer/*", "/api/v1/beerUpc/*")
                 //redirect to beerService which is running on 'localhost:8080'
-                .uri("http://localhost:8080")
+                //lb - for load Balance
+                .uri("lb://micro-02-mssc-beer-service")
                 .id("beer-service"))
             .route(r -> r.path("/api/v1/customers/**")
-                .uri("http://localhost:8081")
+                .uri("lb://micro-05-mssc-beer-order-service")
                 .id("beer-order-service"))
             .route(r -> r.path("/api/v1/beer/*/inventory")
-                .uri("http://localhost:8082")
+                .uri("lb://micro-06-mssc-beer-inventory-service")
                 .id("beer-inventory-service"))
-
             .build();
     }
 
